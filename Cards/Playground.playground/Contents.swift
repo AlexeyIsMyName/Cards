586e317c208f4bc9a933d9800bc9152f317e439b
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-11-01 19:45:06 +0300

    play with custom views

--- Cards/Playground.playground/Contents.swift
+++ Cards/Playground.playground/Contents.swift
@@ -18,9 +18,21 @@ class MyViewController : UIViewController {
         let view = UIView()
         view.backgroundColor = .white
         self.view = view
-        // круг
-        view.layer.addSublayer(BackSideLine(size: CGSize(width: 200, height: 150),
-                                           fillColor: UIColor.gray.cgColor))
+        
+        // игральная карточка рубашкой вверх
+        let firstCardView = CardView<CircleShape>(
+            frame: CGRect(x: 0, y: 0, width: 120, height: 150),
+            color: .red
+        )
+        self.view.addSubview(firstCardView)
+        
+        // игральная карточка лицевой стороной вверх
+        let secondCardView = CardView<CircleShape>(
+            frame: CGRect(x: 200, y: 0, width: 120, height: 150),
+            color: .red
+        )
+        self.view.addSubview(secondCardView)
+        secondCardView.isFlipped = true
     }
 }
 
@@ -40,7 +52,7 @@ class CircleShape: CAShapeLayer, ShapeLayerProtocol {
         let path = UIBezierPath(arcCenter: CGPoint(x: centerX, y: centerY),
                                 radius: radius,
                                 startAngle: 0,
-                                endAngle: .pi*2,
+                                endAngle: .pi * 2,
                                 clockwise: true)
         path.close()
         // инициализируем созданный путь
@@ -171,6 +183,120 @@ class BackSideLine: CAShapeLayer, ShapeLayerProtocol {
         self.strokeColor = fillColor
         self.lineWidth = 3
         self.lineCap = .round
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+protocol FlippableView: UIView {
+    var isFlipped: Bool { get set }
+    var flipCompletionHandler: ((FlippableView) -> Void)? { get set }
+    func flip()
+}
+
+class CardView<ShapeType: ShapeLayerProtocol>: UIView, FlippableView {
+    var isFlipped: Bool = false {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    var flipCompletionHandler: ((FlippableView) -> Void)?
+    func flip() {}
+    
+    // радиус закругления
+    var cornerRadius = 20
+    
+    // внутренний отступ представления
+    private let margin: Int = 10
+    
+    // представление с лицевой стороной карты
+    lazy var frontSideView: UIView = self.getFrontSideView()
+    // представление с обратной стороной карты
+    lazy var backSideView: UIView = self.getBackSideView()
+    
+    
+    // возвращает представление для лицевой стороны карточки
+    private func getFrontSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        let shapeView = UIView(
+            frame: CGRect(x: margin,
+                          y: margin,
+                          width: Int(self.bounds.width)-margin * 2,
+                          height: Int(self.bounds.height)-margin * 2)
+        )
+        
+        view.addSubview(shapeView)
+        
+        // создание слоя с фигурой
+        let shapeLayer = ShapeType(size: shapeView.frame.size,
+                                   fillColor: color.cgColor)
+        
+        shapeView.layer.addSublayer(shapeLayer)
+        
+        return view
+    }
+    
+    // возвращает вью для обратной стороны карточки
+    private func getBackSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        //выбор случайного узора для рубашки
+        switch ["circle", "line"].randomElement()! {
+        case "circle":
+            let layer = BackSideCircle(size: self.bounds.size,
+                                       fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        case "line":
+            let layer = BackSideLine(size: self.bounds.size,
+                                     fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        default:
+            break
+        }
+        return view
+    }
+    
+    // цвет фигуры
+    var color: UIColor!
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        self.color = color
+        if isFlipped {
+            self.addSubview(backSideView)
+            self.addSubview(frontSideView)
+        } else {
+            self.addSubview(frontSideView)
+            self.addSubview(backSideView)
+        }
+        setupBorders()
+    }
+    
+    // настройка границ
+    private func setupBorders() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = CGFloat(cornerRadius)
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    override func draw(_ rect: CGRect) {
+        // удаляем добавленные ранее дочерние представления
+        backSideView.removeFromSuperview()
+        frontSideView.removeFromSuperview()
+        // добавляем новые представления
+        if isFlipped {
+            self.addSubview(backSideView)
+            self.addSubview(frontSideView)
+            
+        } else {
+            self.addSubview(frontSideView)
+            self.addSubview(backSideView)
+        }
     }
     
     required init?(coder: NSCoder) {
