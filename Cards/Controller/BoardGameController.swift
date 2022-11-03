8c7edcf3ef2127ba6a23ec8574c0ef2a639a7ddc
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-11-03 13:35:53 +0300

    add getNewGame method to BoardGameController

--- Cards/Controller/BoardGameController.swift
+++ Cards/Controller/BoardGameController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class BoardGameController: UIViewController {
+    
+    // количество пар уникальных карточек
+    var cardsPairsCounts = 8
+    // сущность "Игра"
+    lazy var game: Game = getNewGame()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func getNewGame() -> Game {
+        let game = Game()
+        game.cardsCount = self.cardsPairsCounts
+        game.generateCards()
+        return game
     }
-    */
-
 }
